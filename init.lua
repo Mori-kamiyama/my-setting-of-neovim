769039f16ad64485b04ee535b42f9513f57d6416
new-umechan <ryu.shamoji@gmail.com>
2024-11-15 09:17:51 +0900

    gitコメントが見やすくなりました。icebergからカスタマイズされてきたかも。

--- init.lua
+++ init.lua
@@ -4,6 +4,8 @@ require('ume/basic_config')
 require('ume/setup_plugin')
 vim.cmd('colorscheme iceberg')
 require('ume/plugins/nvim_tree')
+
+-- icebergにある若干の不満を解決
 vim.api.nvim_set_hl(0, "WinSeparator", { bg = "#0f1117", fg = "#0f1117" })
 vim.api.nvim_set_hl(0, 'WarningMsg', { fg = '#6A708B', bg = 'NONE' })
 vim.api.nvim_set_hl(0, 'DiagnosticWarn', { fg = '#6A708B', bg = 'NONE' })
--- lua/ume/plugins/ale.lua
+++ lua/ume/plugins/ale.lua
@@ -1,3 +1,4 @@
+-- この設定ないとvimになるよ
 vim.g.ale_sign_column_always = 0
 vim.g.ale_set_signs = 0
 
